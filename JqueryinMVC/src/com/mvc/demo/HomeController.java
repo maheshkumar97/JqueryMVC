11302021fc63dac8fca9184b49362312df11c5c8
Maheshkumar <Maheshkumar@DESKTOP-HMFG54U>
2020-07-23 18:54:27 +0530

    Jquey dailog add to a form and showing the data in dailog

--- JqueryinMVC/src/com/mvc/demo/HomeController.java
+++ JqueryinMVC/src/com/mvc/demo/HomeController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,12 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String showHome() {
-		return "welcome";
+	public String showHome(Model model) {
+		Student theStudent = new Student();
+		model.addAttribute("student", theStudent);
+		return "form";
 	}
 	
-	@RequestMapping("/login")
-	public String login(@RequestParam("name") String name){
+	@RequestMapping("/processForm")
+	public String login(@ModelAttribute("student") Student theStudent){
+		
+		System.out.println(theStudent.getFirstName());
+		
 		
 		return "ui";
 	}
